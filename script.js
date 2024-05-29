7d7b9ec3b4153b17fefccbe18ecd4fd632f473fc
JustinM12345 <159297899+JustinM12345@users.noreply.github.com>
2024-05-29 02:11:42 +0000

    Should be final for science fair

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const URL = "https://teachablemachine.withgoogle.com/models/m9JsFfBs_/";
 var last = "";
 let model, webcam, labelContainer, maxPredictions, labelContainers;
-let lastSpeechTime = 0; 
-const speechDelay = 5000; 
+let lastSpeechTime = 0;
+const speechDelay = 10000;
 let cameraRunning = false;
 
 // Load the image model and setup the webcam
@@ -17,9 +17,9 @@ async function init() {
   maxPredictions = model.getTotalClasses();
 
   // Webcam Setup
-  const flip = true; 
-  webcam = new tmImage.Webcam(300, 300, flip); 
-  await webcam.setup(); 
+  const flip = true;
+  webcam = new tmImage.Webcam(300, 300, flip);
+  await webcam.setup();
   await webcam.play();
   window.requestAnimationFrame(loop);
 
@@ -28,7 +28,7 @@ async function init() {
   document.getElementById("webcam-container").appendChild(webcam.canvas);
   labelContainer = document.getElementById("label-container");
   labelContainers = document.getElementById("dev-stats");
-  for (let i = 0; i < maxPredictions; i++) { 
+  for (let i = 0; i < maxPredictions; i++) {
 
     labelContainer.appendChild(document.createElement("div"));
     labelContainers.appendChild(document.createElement("div"));
@@ -43,7 +43,7 @@ async function stopping() {
 }
 
 async function loop() {
-  webcam.update(); 
+  webcam.update();
   await predict();
   await in_depth_stats();
   window.requestAnimationFrame(loop);
@@ -60,21 +60,19 @@ async function in_depth_stats() {
   }
 }
 
-// run the webcam image through the image model
+// This function will run whatever the webcam sees through Teachable Machine
 async function predict() {
-  const prediction = await model.predict(webcam.canvas);
-  most = 0;
+  const prediction = await model.predict(webcam.canvas); 
+  most = 0; 
   for (let i = 0; i < maxPredictions; i++) {
     if (prediction[i].probability.toFixed(1) * 100 >= most) {
       most = prediction[i].probability.toFixed(1) * 100
     }
 
     const classPrediction = prediction[i].className + ": " + (prediction[i].probability.toFixed(1) * 100 + "%");
-    document.getElementById("dev-stats").append;
     if (prediction[i].probability.toFixed(1) * 100 >= 90) {
-      document.getElementById("progress").value = prediction[i].probability.toFixed(1) * 100;
-      document.getElementById("progress").innerText = prediction[i].probability.toFixed(1) * 100 + "%";
       labelContainer.childNodes[i].innerHTML = classPrediction;
+      document.getElementById("progress").value = most;
 
       // Specific Messages depending on name of class
       const currentTime = new Date().getTime();
@@ -82,62 +80,68 @@ async function predict() {
 
         if (prediction[i].className == "None") {
           last = prediction[i].className;
-        } 
-          
-        else if (prediction[i].className == "Car or Bus") {
+        }
+
+        else if (prediction[i].className == "Car In Front Of Camera") {
           last = prediction[i].className;
           var msg = new SpeechSynthesisUtterance();
           msg.text = "Be careful! There is a car or bus in front of the camera";
           window.speechSynthesis.speak(msg);
-          lastSpeechTime = currentTime; 
-        } 
-          
+          lastSpeechTime = currentTime;
+        }
+
+        else if (prediction[i].className == "High Voltage") {
+          last = prediction[i].className;
+          var msg = new SpeechSynthesisUtterance();
+          msg.text = "Be careful! There is a high voltage sign in front of the camera";
+          window.speechSynthesis.speak(msg);
+          lastSpeechTime = currentTime;
+        }
+
         else {
           last = prediction[i].className;
           var msg = new SpeechSynthesisUtterance();
           msg.text = "Be careful! There is a " + prediction[i].className + " in front of the camera";
           window.speechSynthesis.speak(msg);
-          lastSpeechTime = currentTime; 
+          lastSpeechTime = currentTime;
         }
       }
       // Shows speech timer and how much time is left
       var speech_timer = document.getElementById("speech_timer");
-      speech_timer.innerHTML = currentTime - lastSpeechTime;
-      
+      speech_timer.innerHTML = "Time since last detection (ms): " + (currentTime - lastSpeechTime);
+
     } else {
       labelContainer.childNodes[i].innerHTML = "";
     }
   }
-  document.getElementById("progress").value = most;
+  
 }
 
 // VOICE RECOGNITION AND VOICE COMMANDS SECTION
-
-var speak = document.getElementById("speak");
 var textarea = document.getElementById("textarea");
 var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-var recognition = new SpeechRecognition();
+var recognition = new SpeechRecognition(); 
 recognition.continuous = true;
-recognition.interimResults = true; 
+recognition.interimResults = true;
 
 // Turn on camera w/ speech
 function turnOn() {
   init();
+  recognition.stop();
   var msg = new SpeechSynthesisUtterance();
   msg.text = "Starting Camera";
   window.speechSynthesis.speak(msg);
   cameraRunning = true;
-  recognition.stop();
 }
 
 // Turn off camera w/ speech
 function turnOff() {
   stopping();
+  recognition.stop();
   var msg = new SpeechSynthesisUtterance();
   msg.text = "Stopping Camera";
   window.speechSynthesis.speak(msg);
   cameraRunning = false;
-  recognition.stop();
 }
 
 // Constantly running speech recognition
@@ -163,3 +167,4 @@ function speech() {
   }
 }
 speech();
+
